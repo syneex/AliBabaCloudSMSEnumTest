82ca8385d8e3a458449d747fc621666586034943
Samuel Sülzle <samuel.suelzle@plan-b-gmbh.com>
2021-08-05 15:58:38 +0200

    Added all Templates test

--- EnumTestJsonConvert/Program.cs
+++ EnumTestJsonConvert/Program.cs
@@ -10,14 +10,43 @@ namespace EnumTestJsonConvert
         static void Main(string[] args)
         {
             var singleTemplateString = File.ReadAllText("./Samples/singleTemplate.json");
-            // var allTemplateString = File.ReadAllText("./Samples/allTemplates.json");
 
-            AliCloudChinaSMSTemplate singleTemplate = JsonConvert.DeserializeObject<AliCloudChinaSMSTemplateOutput>(singleTemplateString);
+            AliCloudChinaSMSTemplateOutput singleTemplate = JsonConvert.DeserializeObject<AliCloudChinaSMSTemplateOutput>(singleTemplateString);
+            JObject directSingleTemplate = JObject.Parse(JsonConvert.SerializeObject(singleTemplate));
 
-            Console.WriteLine(".ToString():");
-            Console.WriteLine(singleTemplate.ToString());
-            Console.WriteLine("\nJsonConvert.Serialize");
-            Console.WriteLine(JsonConvert.SerializeObject(singleTemplate));
+            Console.WriteLine("------------------------------------------- [START] Single Template -------------------------------------------");
+            Console.WriteLine("Converted using '.ToString()':");
+            Console.WriteLine(singleTemplate.ToString() + "\n");
+
+            Console.WriteLine("Converted using '.SerializeObject()':");
+            Console.WriteLine(JsonConvert.SerializeObject(singleTemplate) + "\n");
+            
+            Console.WriteLine("Output directly:");
+            Console.WriteLine(directSingleTemplate);
+            Console.WriteLine("------------------------------------------- [END] Single Template -------------------------------------------");
+
+
+
+            var allTemplateString = File.ReadAllText("./Samples/allTemplates.json");
+            JArray allTemplates = JArray.Parse(allTemplateString);
+            JArray allTemplatesConverted = new JArray();
+            foreach (JObject item in allTemplates)
+            {
+                AliCloudChinaSMSTemplateOutput tempItem = JsonConvert.DeserializeObject<AliCloudChinaSMSTemplateOutput>(item.ToString());
+                allTemplatesConverted.Add(JObject.Parse(JsonConvert.SerializeObject(tempItem)));
+            }
+
+
+            Console.WriteLine("------------------------------------------- [START] All Templates -------------------------------------------");
+            Console.WriteLine("Converted using '.ToString()':");
+            Console.WriteLine(allTemplates.ToString() + "\n");
+
+            Console.WriteLine("Converted using '.SerializeObject()':");
+            Console.WriteLine(JsonConvert.SerializeObject(allTemplates) + "\n");
+
+            Console.WriteLine("Output directly:");
+            Console.WriteLine(allTemplatesConverted);
+            Console.WriteLine("------------------------------------------- [END] All Templates -------------------------------------------");
 
             Console.ReadKey();
         }
